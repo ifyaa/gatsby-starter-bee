28b02756ffaa6a0adb5f80f4a2e60bee2def175b
nasa2135 <nasa2135@gmail.com>
2020-07-25 10:42:52 +0900

    why??? date error

--- content/blog/그외/second.md
+++ content/blog/그외/second.md
@@ -1,16 +1,18 @@
 ---
-title: '코드에러 해결'
+title: "코드에러 해결"
 date: 2020-07-21
-category: '그외'
+category: "그외"
 draft: false
 ---
+
 코드가 문제발생
-netlify에 업데이터 에러가 발생했고   clone로 컴에 설치한후  thumbnail를 실행불가로 처리한 후 push하지 제대로 실행되었다..왜 이런 문제가 발생했는지는 모르겠다 ...이미지 문제같은데...
+netlify에 업데이터 에러가 발생했고 clone로 컴에 설치한후 thumbnail를 실행불가로 처리한 후 push하지 제대로 실행되었다..왜 이런 문제가 발생했는지는 모르겠다 ...이미지 문제같은데...
+
 ```js
       html
       frontmatter {
         title
-        date(formatString: "MMMM DD, YYYY")
+        // date(formatString: "MMMM DD, YYYY")
         # thumbnail {
         #   childImageSharp {
         #     fixed(width: 800) {
@@ -22,13 +24,16 @@ netlify에 업데이터 에러가 발생했고   clone로 컴에 설치한후  t
     }
   }
 ```
+
 ![](https://i.ibb.co/zZ1sTbz/image.png)Second Post'
 date: 2019-2-3 16:21:13
 category: 'development'
 draft: false
+
 ---
 
 Second Post
+
 <!--stackedit_data:
 eyJoaXN0b3J5IjpbLTExODU4MjMwNTYsLTEyMTI2OTE2NzJdfQ
 ==
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,48 +1,48 @@
-import { graphql } from 'gatsby'
-import _ from 'lodash'
-import React, { useMemo } from 'react'
-import { Bio } from '../components/bio'
-import { Category } from '../components/category'
-import { Contents } from '../components/contents'
-import { Head } from '../components/head'
-import { HOME_TITLE } from '../constants'
-import { useCategory } from '../hooks/useCategory'
-import { useIntersectionObserver } from '../hooks/useIntersectionObserver'
-import { useRenderedCount } from '../hooks/useRenderedCount'
-import { useScrollEvent } from '../hooks/useScrollEvent'
-import { Layout } from '../layout'
-import * as Dom from '../utils/dom'
-import * as EventManager from '../utils/event-manager'
+import { graphql } from "gatsby";
+import _ from "lodash";
+import React, { useMemo } from "react";
+import { Bio } from "../components/bio";
+import { Category } from "../components/category";
+import { Contents } from "../components/contents";
+import { Head } from "../components/head";
+import { HOME_TITLE } from "../constants";
+import { useCategory } from "../hooks/useCategory";
+import { useIntersectionObserver } from "../hooks/useIntersectionObserver";
+import { useRenderedCount } from "../hooks/useRenderedCount";
+import { useScrollEvent } from "../hooks/useScrollEvent";
+import { Layout } from "../layout";
+import * as Dom from "../utils/dom";
+import * as EventManager from "../utils/event-manager";
 
-const BASE_LINE = 80
+const BASE_LINE = 80;
 
 function getDistance(currentPos) {
-  return Dom.getDocumentHeight() - currentPos
+  return Dom.getDocumentHeight() - currentPos;
 }
 
 export default ({ data, location }) => {
-  const { siteMetadata } = data.site
-  const { countOfInitialPost } = siteMetadata.configs
-  const posts = data.allMarkdownRemark.edges
+  const { siteMetadata } = data.site;
+  const { countOfInitialPost } = siteMetadata.configs;
+  const posts = data.allMarkdownRemark.edges;
   const categories = useMemo(
     () => _.uniq(posts.map(({ node }) => node.frontmatter.category)),
     []
-  )
-  const [count, countRef, increaseCount] = useRenderedCount()
-  const [category, selectCategory] = useCategory()
+  );
+  const [count, countRef, increaseCount] = useRenderedCount();
+  const [category, selectCategory] = useCategory();
 
-  useIntersectionObserver()
+  useIntersectionObserver();
   useScrollEvent(() => {
-    const currentPos = window.scrollY + window.innerHeight
-    const isTriggerPos = () => getDistance(currentPos) < BASE_LINE
+    const currentPos = window.scrollY + window.innerHeight;
+    const isTriggerPos = () => getDistance(currentPos) < BASE_LINE;
     const doesNeedMore = () =>
-      posts.length > countRef.current * countOfInitialPost
+      posts.length > countRef.current * countOfInitialPost;
 
     return EventManager.toFit(increaseCount, {
       dismissCondition: () => !isTriggerPos(),
       triggerCondition: () => isTriggerPos() && doesNeedMore(),
-    })()
-  })
+    })();
+  });
 
   return (
     <Layout location={location} title={siteMetadata.title}>
@@ -60,8 +60,8 @@ export default ({ data, location }) => {
         category={category}
       />
     </Layout>
-  )
-}
+  );
+};
 
 export const pageQuery = graphql`
   query {
@@ -84,7 +84,7 @@ export const pageQuery = graphql`
             slug
           }
           frontmatter {
-            date(formatString: "MMMM DD, YYYY")
+            date
             title
             category
             draft
@@ -93,4 +93,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
